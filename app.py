4624af0c7076226ed9e8690873cd1d652ea8af62
Thales Rudolph <rudolphthales1@gmail.com>
2024-04-26 12:38:59 -0300

    fix: fixed time of experience from 4 to 5

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ tab1.write(
 - \n
                                                                          
                                                                         
-- ✔️ 4 anos de experiência em processo ETL de pipeline de dados;
+- ✔️ 5 anos de experiência em processo ETL de pipeline de dados;
 - ✔️ Conhecimento avançado de Excel e Python;
 - ✔️ Entendimento de técnicas e regulamentos normativos de dados;
 - ✔️ Facilidade e velocidade em aprender;
